64417d740e1e24987467213ac1d95401ecb1d2c0
memo figueredo <memof80@gmail.com>
2022-05-20 17:33:23 -0500

    added intercator

--- MovieApp/Modules/DetailMovies/Interactor/AnyDetailMovieInteractorProtocols.swift
+++ MovieApp/Modules/DetailMovies/Interactor/AnyDetailMovieInteractorProtocols.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+protocol AnyDetailMovieInteractorInputProtocol {
+    
+    var repository: AnyDetailMovieRespositoryInputProtocol? { get set }
+    var presenter: AnyGetDetailMovieInteractorOutputProtocol? { get set }
+    
+    func execute(idMovie: Int)
+}
+
+protocol AnyGetDetailMovieInteractorOutputProtocol: AnyObject {
+    func didRetriveDetailMovie(_ detailMovie: DetailMovie)
+    func didGetError(_ error: CustomError)
+}
+
+
--- MovieApp/Modules/DetailMovies/Interactor/DetailMovieInteractor.swift
+++ MovieApp/Modules/DetailMovies/Interactor/DetailMovieInteractor.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+final class GetDeatilMovieInteractor: AnyDetailMovieInteractorInputProtocol {
+    
+    var repository: AnyDetailMovieRespositoryInputProtocol?
+    
+    var presenter: AnyGetDetailMovieInteractorOutputProtocol?
+    
+    func execute(idMovie: Int) {
+        repository?.getDetailMovie(idMovie: idMovie)
+    }
+}
+
+extension GetDeatilMovieInteractor: AnyDetailMovieRespositoryOutputProtocol {
+    
+    func didRetriveDetailMovie(_ detailMovie: DetailMovie) {
+        presenter?.didRetriveDetailMovie(detailMovie)
+    }
+    
+    func didGetError(_ error: CustomError) {
+        presenter?.didGetError(error)
+    }
+    
+    
+}
